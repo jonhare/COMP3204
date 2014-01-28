94f89e34a30f635f1117339b8aa228c2abc57ce9
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2014-01-28 23:57:39 +0000

    added matrix

--- src/main/java/uk/ac/soton/ecs/comp3005/CovarianceDemo.java
+++ src/main/java/uk/ac/soton/ecs/comp3005/CovarianceDemo.java
@@ -2,8 +2,10 @@ package uk.ac.soton.ecs.comp3005;
 
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.TextField;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -35,6 +37,10 @@ public class CovarianceDemo implements Slide {
 	private JSlider xxSlider;
 	private JSlider yySlider;
 	private JSlider xySlider;
+	private TextField xxField;
+	private TextField xyField;
+	private TextField yxField;
+	private TextField yyField;
 
 	@Override
 	public Component getComponent(int width, int height) throws IOException {
@@ -52,7 +58,6 @@ public class CovarianceDemo implements Slide {
 		imageComp.setAllowZoom(false);
 		imageComp.setAllowPanning(false);
 		base.add(imageComp);
-		updateImage();
 
 		xxSlider = new JSlider();
 		yySlider = new JSlider();
@@ -82,28 +87,68 @@ public class CovarianceDemo implements Slide {
 			}
 		});
 
+		final Font fnt = Font.decode("Monaco-48");
+
 		final GridBagConstraints c = new GridBagConstraints();
+		final JPanel matrix = new JPanel(new GridBagLayout());
+		c.gridwidth = 1;
+		c.gridheight = 1;
+		c.gridy = 0;
+		c.gridx = 0;
+		matrix.add(xxField = new TextField(5), c);
+		xxField.setFont(fnt);
+		xxField.setEditable(false);
+		c.gridx = 1;
+		matrix.add(xyField = new TextField(5), c);
+		xyField.setFont(fnt);
+		xyField.setEditable(false);
+		c.gridy = 1;
+		c.gridx = 0;
+		matrix.add(yxField = new TextField(5), c);
+		yxField.setFont(fnt);
+		yxField.setEditable(false);
+		c.gridx = 1;
+		matrix.add(yyField = new TextField(5), c);
+		yyField.setFont(fnt);
+		yyField.setEditable(false);
 
 		final JPanel controls = new JPanel(new GridBagLayout());
-		c.gridy = 0;
 		c.gridwidth = 1;
-		controls.add(new JLabel("XX"), c);
-		controls.add(xxSlider, c);
+		c.gridheight = 1;
+		c.gridy = 0;
+		controls.add(matrix, c);
 
 		c.gridy = 1;
-		controls.add(new JLabel("YY"), c);
-		controls.add(yySlider, c);
+		c.gridx = 0;
+		controls.add(new JLabel("XX"), c);
+		c.gridx = 1;
+		controls.add(xxSlider, c);
 
 		c.gridy = 2;
+		c.gridx = 0;
+		controls.add(new JLabel("YY"), c);
+		c.gridx = 1;
+		controls.add(yySlider, c);
+
+		c.gridy = 3;
+		c.gridx = 0;
 		controls.add(new JLabel("XY"), c);
+		c.gridx = 1;
 		controls.add(xySlider, c);
 
 		base.add(controls);
+
+		updateImage();
 
 		return base;
 	}
 
 	private void updateImage() {
+		xxField.setText(String.format("%2.3f", covariance.get(0, 0)));
+		xyField.setText(String.format("%2.3f", covariance.get(0, 1)));
+		yxField.setText(String.format("%2.3f", covariance.get(1, 0)));
+		yyField.setText(String.format("%2.3f", covariance.get(1, 1)));
+
 		image.fill(RGBColour.WHITE);
 
 		final Ellipse e = EllipseUtilities.ellipseFromCovariance(image.getWidth() / 2, image.getHeight() / 2, covariance,
