5bffd07007fcb17e458cd9f4cbaa360f158bd5b0
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2014-02-05 22:10:51 +0000

    working on more demos

---
+++ src/main/java/uk/ac/soton/ecs/comp3005/l3/FaceDatasetDemo.java
@@ -0,0 +1,60 @@
+package uk.ac.soton.ecs.comp3005.l3;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.io.IOException;
+
+import javax.swing.JPanel;
+
+import org.openimaj.content.slideshow.Slide;
+import org.openimaj.content.slideshow.SlideshowApplication;
+import org.openimaj.data.dataset.VFSGroupDataset;
+import org.openimaj.image.DisplayUtilities.ImageComponent;
+import org.openimaj.image.FImage;
+import org.openimaj.image.ImageUtilities;
+
+public class FaceDatasetDemo implements Slide {
+
+	@Override
+	public Component getComponent(int width, int height) throws IOException {
+		final VFSGroupDataset<FImage> dataset = new VFSGroupDataset<FImage>("zip:"
+				+ getClass().getResource("att_faces.zip"),
+				ImageUtilities.FIMAGE_READER);
+
+		final JPanel outer = new JPanel();
+		outer.setOpaque(false);
+		outer.setPreferredSize(new Dimension(width, height));
+		outer.setLayout(new GridBagLayout());
+
+		final JPanel base = new JPanel();
+		base.setOpaque(false);
+		base.setPreferredSize(new Dimension(width, height - 50));
+		base.setLayout(new FlowLayout());
+
+		for (int i = 0; i < 60; i++) {
+			System.out.println("here");
+			final FImage img = dataset.getRandomInstance();
+			final ImageComponent ic = new ImageComponent(true);
+			ic.setAllowPanning(false);
+			ic.setAllowZoom(false);
+			ic.setShowPixelColours(false);
+			ic.setShowXYPosition(false);
+			ic.setImage(ImageUtilities.createBufferedImageForDisplay(img));
+			base.add(ic);
+		}
+		outer.add(base);
+
+		return outer;
+	}
+
+	@Override
+	public void close() {
+		// do nothing
+	}
+
+	public static void main(String[] args) throws IOException {
+		new SlideshowApplication(new FaceDatasetDemo(), 1024, 768);
+	}
+}
---
+++ src/main/java/uk/ac/soton/ecs/comp3005/l3/MeanFaceDemo.java
@@ -0,0 +1,56 @@
+package uk.ac.soton.ecs.comp3005.l3;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.io.IOException;
+
+import javax.swing.JPanel;
+
+import org.openimaj.content.slideshow.Slide;
+import org.openimaj.content.slideshow.SlideshowApplication;
+import org.openimaj.data.dataset.VFSGroupDataset;
+import org.openimaj.image.DisplayUtilities.ImageComponent;
+import org.openimaj.image.FImage;
+import org.openimaj.image.ImageUtilities;
+import org.openimaj.image.processing.resize.ResizeProcessor;
+
+public class MeanFaceDemo implements Slide {
+
+	@Override
+	public Component getComponent(int width, int height) throws IOException {
+		final VFSGroupDataset<FImage> dataset = new VFSGroupDataset<FImage>("zip:"
+				+ getClass().getResource("att_faces.zip"),
+				ImageUtilities.FIMAGE_READER);
+
+		final FImage img = dataset.getRandomInstance().fill(0f);
+		for (final FImage i : dataset) {
+			img.addInplace(i);
+		}
+		img.divideInplace(dataset.numInstances()).processInplace(new ResizeProcessor(6.0f));
+
+		final JPanel outer = new JPanel();
+		outer.setOpaque(false);
+		outer.setPreferredSize(new Dimension(width, height));
+		outer.setLayout(new GridBagLayout());
+
+		final ImageComponent ic = new ImageComponent(true);
+		ic.setAllowPanning(false);
+		ic.setAllowZoom(false);
+		ic.setShowPixelColours(false);
+		ic.setShowXYPosition(false);
+		ic.setImage(ImageUtilities.createBufferedImageForDisplay(img));
+		outer.add(ic);
+
+		return outer;
+	}
+
+	@Override
+	public void close() {
+		// do nothing
+	}
+
+	public static void main(String[] args) throws IOException {
+		new SlideshowApplication(new MeanFaceDemo(), 1024, 768);
+	}
+}
