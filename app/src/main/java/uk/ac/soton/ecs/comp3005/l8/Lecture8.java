9eb8333a32a4e076d3d2d7d206893f252577b938
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2014-04-28 13:19:25 +0100

    finishing l8

--- app/src/main/java/uk/ac/soton/ecs/comp3005/l8/Lecture8.java
+++ app/src/main/java/uk/ac/soton/ecs/comp3005/l8/Lecture8.java
@@ -20,10 +20,28 @@ public class Lecture8 {
 	public static void main(String[] args) throws IOException {
 		final List<Slide> slides = new ArrayList<Slide>();
 
-		for (int i = 1; i <= 24; i++)
+		for (int i = 1; i <= 33; i++)
 			slides.add(new PictureSlide(Lecture8.class.getResource(String.format("l8.%03d.jpg", i))));
 
-		// slides.add(new NarrowBaselineTemplatesDemo());
+		slides.add(new LineRANSACDemo());
+
+		for (int i = 34; i <= 36; i++)
+			slides.add(new PictureSlide(Lecture8.class.getResource(String.format("l8.%03d.jpg", i))));
+
+		slides.add(new SIFTMatchingDemo());
+
+		for (int i = 37; i <= 38; i++)
+			slides.add(new PictureSlide(Lecture8.class.getResource(String.format("l8.%03d.jpg", i))));
+
+		slides.add(new SFMDemo());
+
+		for (int i = 39; i <= 40; i++)
+			slides.add(new PictureSlide(Lecture8.class.getResource(String.format("l8.%03d.jpg", i))));
+
+		slides.add(new SpectrogramDemo());
+
+		for (int i = 41; i <= 57; i++)
+			slides.add(new PictureSlide(Lecture8.class.getResource(String.format("l8.%03d.jpg", i))));
 
 		new SlideshowApplication(slides, 1024, 768, Utils.BACKGROUND_IMAGE);
 	}
