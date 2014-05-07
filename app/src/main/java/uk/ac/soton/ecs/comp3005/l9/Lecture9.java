a8818741d7339228860b3931fb105ebf5435a057
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2014-05-07 14:25:12 +0100

    finishing le9

--- app/src/main/java/uk/ac/soton/ecs/comp3005/l9/Lecture9.java
+++ app/src/main/java/uk/ac/soton/ecs/comp3005/l9/Lecture9.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.openimaj.content.slideshow.PictureSlide;
 import org.openimaj.content.slideshow.Slide;
 import org.openimaj.content.slideshow.SlideshowApplication;
+import org.openimaj.content.slideshow.VideoSlide;
+import org.openimaj.video.VideoDisplay.EndAction;
 
 import uk.ac.soton.ecs.comp3005.utils.Utils;
 import uk.ac.soton.ecs.comp3005.utils.annotations.JvmArgs;
@@ -19,38 +22,19 @@ public class Lecture9 {
 	public static void main(String[] args) throws IOException {
 		final List<Slide> slides = new ArrayList<Slide>();
 
-		// for (int i = 1; i <= 33; i++)
-		// slides.add(new
-		// PictureSlide(Lecture9.class.getResource(String.format("l8.%03d.jpg",
-		// i))));
-		//
-		// slides.add(new LineRANSACDemo());
-		//
-		// for (int i = 34; i <= 36; i++)
-		// slides.add(new
-		// PictureSlide(Lecture9.class.getResource(String.format("l8.%03d.jpg",
-		// i))));
-		//
-		// slides.add(new SIFTMatchingDemo());
-		//
-		// for (int i = 37; i <= 38; i++)
-		// slides.add(new
-		// PictureSlide(Lecture9.class.getResource(String.format("l8.%03d.jpg",
-		// i))));
-		//
-		// slides.add(new SFMDemo());
-		//
-		// for (int i = 39; i <= 40; i++)
-		// slides.add(new
-		// PictureSlide(Lecture9.class.getResource(String.format("l8.%03d.jpg",
-		// i))));
-		//
-		// slides.add(new SpectrogramDemo());
-		//
-		// for (int i = 41; i <= 57; i++)
-		// slides.add(new
-		// PictureSlide(Lecture9.class.getResource(String.format("l8.%03d.jpg",
-		// i))));
+		for (int i = 1; i <= 28; i++)
+			slides.add(new PictureSlide(Lecture9.class.getResource(String.format("l9.%03d.jpg", i))));
+
+		slides.add(new BoVWHistogramDemo());
+
+		for (int i = 29; i <= 37; i++)
+			slides.add(new PictureSlide(Lecture9.class.getResource(String.format("l9.%03d.jpg", i))));
+
+		slides.add(new VideoSlide(Lecture9.class.getResource("TrentoDuomo.mov"), Utils.BACKGROUND_IMAGE_URL,
+				EndAction.PAUSE_AT_END));
+
+		for (int i = 38; i <= 45; i++)
+			slides.add(new PictureSlide(Lecture9.class.getResource(String.format("l9.%03d.jpg", i))));
 
 		new SlideshowApplication(slides, 1024, 768, Utils.BACKGROUND_IMAGE);
 	}
