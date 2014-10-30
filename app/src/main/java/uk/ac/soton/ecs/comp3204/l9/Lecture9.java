b51573ad25347e0391d22c95797bce958ab50ba9
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2014-10-30 09:48:14 +0000

    prepping L9

--- app/src/main/java/uk/ac/soton/ecs/comp3204/l9/Lecture9.java
+++ app/src/main/java/uk/ac/soton/ecs/comp3204/l9/Lecture9.java
@@ -14,11 +14,9 @@ import uk.ac.soton.ecs.comp3204.utils.Utils;
 import uk.ac.soton.ecs.comp3204.utils.annotations.JvmArgs;
 import uk.ac.soton.ecs.comp3204.utils.annotations.Lecture;
 
-/*
-@Lecture(title = "L9: Image Search and Classification",
-		handoutsURL = "http://jonhare.github.io/COMP3204/handouts/pdf/L9-imagesearchclass.pdf",
-		slidesURL = "http://jonhare.github.io/COMP3204/lectures/pdf/L9-imagesearchclass.pdf")
-*/
+@Lecture(title = "L9: Image Search and Bags of Visual Words",
+		handoutsURL = "http://jonhare.github.io/COMP3204/handouts/pdf/L9-imagesearch.pdf",
+		slidesURL = "http://jonhare.github.io/COMP3204/lectures/pdf/L9-imagesearch.pdf")
 @JvmArgs(vmArguments = "-Xmx1G")
 public class Lecture9 {
 	public static void main(String[] args) throws IOException {
@@ -35,7 +33,7 @@ public class Lecture9 {
 		slides.add(new VideoSlide(Lecture9.class.getResource("TrentoDuomo.mov"), Utils.BACKGROUND_IMAGE_URL,
 				EndAction.PAUSE_AT_END));
 
-		for (int i = 38; i <= 45; i++)
+		for (int i = 38; i <= 39; i++)
 			slides.add(new PictureSlide(Lecture9.class.getResource(String.format("l9.%03d.jpg", i))));
 
 		new SlideshowApplication(slides, 1024, 768, Utils.BACKGROUND_IMAGE);
