8108a7c347fc0a5c861c3794a3aae5eff6b79f21
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2014-11-17 17:28:51 +0000

    l10

---
+++ app/src/main/java/uk/ac/soton/ecs/comp3204/l10/Lecture10.java
@@ -0,0 +1,37 @@
+package uk.ac.soton.ecs.comp3204.l10;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openimaj.content.slideshow.PictureSlide;
+import org.openimaj.content.slideshow.Slide;
+import org.openimaj.content.slideshow.SlideshowApplication;
+
+import uk.ac.soton.ecs.comp3204.l2.KNNDemo;
+import uk.ac.soton.ecs.comp3204.l2.LinearClassifierDemo;
+import uk.ac.soton.ecs.comp3204.utils.Utils;
+import uk.ac.soton.ecs.comp3204.utils.annotations.JvmArgs;
+import uk.ac.soton.ecs.comp3204.utils.annotations.Lecture;
+
+@Lecture(title = "L10: Image Classification and Auto Annotation",
+		handoutsURL = "http://jonhare.github.io/COMP3204/handouts/pdf/L10-classification.pdf",
+		slidesURL = "http://jonhare.github.io/COMP3204/lectures/pdf/L10-classification.pdf")
+@JvmArgs(vmArguments = "-Xmx1G")
+public class Lecture10 {
+	public static void main(String[] args) throws IOException {
+		final List<Slide> slides = new ArrayList<Slide>();
+
+		for (int i = 1; i <= 3; i++)
+			slides.add(new PictureSlide(Lecture10.class.getResource(String.format("l10.%03d.jpg", i))));
+
+		slides.add(new LinearClassifierDemo());
+		slides.add(new PictureSlide(Lecture10.class.getResource(String.format("l10.%03d.jpg", 4))));
+		slides.add(new KNNDemo());
+
+		for (int i = 5; i <= 38; i++)
+			slides.add(new PictureSlide(Lecture10.class.getResource(String.format("l10.%03d.jpg", i))));
+
+		new SlideshowApplication(slides, 1024, 768, Utils.BACKGROUND_IMAGE);
+	}
+}
